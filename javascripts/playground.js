f874aaa4a736e5f5382f47604515168dbb73fb4e
june29 <june29.jp@gmail.com>
2011-02-07 21:51:44 +0900

    guragura 120 seconds later

--- javascripts/playground.js
+++ javascripts/playground.js
@@ -1,18 +1,25 @@
 $(document).ready(function() {
+  var READY = 120;
+
   setTimeout(function() {
-    var style = document.createElement('style');
-    document.body.appendChild(style);
-    style.textContent = "img, canvas, video {\n" + "-moz-transform: rotate(0deg);\n" + "  }";
+    var body = $(document.body);
+    var notification = $('<div />').attr('id', 'notification')
+                                   .text('アクセスから' + READY + '秒が経過しました。揺れます。');
+
+    var style = $('<style />').text('img, canvas, video, a, iframe { -moz-transform: rotate(0deg); }').appendTo(body);
 
     var offset = {};
     window.addEventListener("MozOrientation", function(data) {
+      if (!document.getElementById('notification')) {
+        notification.hide().appendTo(body).slideDown(3000);
+      }
 
       if (!offset.x) offset.x = data.x;
       if (!offset.y) offset.y = data.y;
       if (!offset.z) offset.z = data.z;
       var radian = -1 * (data.x - offset.x) * 90;
 
-      style.sheet.cssRules[0].style.MozTransform = 'rotate(' + radian + 'deg)';
+      style.get(0).sheet.cssRules[0].style.MozTransform = 'rotate(' + radian + 'deg)';
     }, true);
-  }, 90 * 1000);
+  }, READY * 1000);
 });
